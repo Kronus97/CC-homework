6145c398ad1eff902e1cc586171fbe0cb3afc2fa
Kronus97 <mihai.soml3a@gmail.com>
2021-10-19 23:33:42 +0300

    feature: added tailwind style and and a new box-shadow style

--- homework2/tailwind.config.js
+++ homework2/tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    boxShadow: {
+      strong: '2px 0 0 0 #888, 0 2px 0 0 #888, 2px 2px 0 0 #888, 2px 0 0 0 #888 inset, 0 2px 0 0 #888 inset;'
+    }
   },
   variants: {
     extend: {},
