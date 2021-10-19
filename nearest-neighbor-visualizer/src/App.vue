819cffdf0feaf3eaaaf428add168154eabce9c17
Kronus97 <mihai.soml3a@gmail.com>
2021-10-19 18:51:35 +0300

    chore: added the initial project template

---
+++ nearest-neighbor-visualizer/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <Nav/>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+import Nav from "./components/Nav.vue";
+import Grid from "./components/Grid.vue";
+
+@Options({
+  components: {
+    Nav,
+    Grid,
+  },
+})
+export default class App extends Vue {}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
---
+++ nearest-neighbor-visualizer/src/components/Grid.vue
@@ -0,0 +1,12 @@
+<template></template>
+
+<script>
+import { Options, Vue } from "vue-class-component";
+
+@Options({})
+export default class Grid extends Vue {
+
+}
+</script>
+
+<style></style>
